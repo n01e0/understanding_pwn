ee3011c062770f31013ace274b34594057d54074
n01e0 <noleo@vanillabeans.mobi>
2020-09-29 13:56:31 +0900

    change val

--- lecture_2/task/src.c
+++ lecture_2/task/src.c
@@ -5,13 +5,13 @@ void win() {
     system("sh");
 }
 
-int zero = 0;
-
 int main() {
+    int zero = 0;
+
     char name[30];
     printf("What's your name?\n>");
     fread(name, 1, 100, stdin);
-    if (zero != 0) {
+    if (zero == 0xdeafbeef) {
         win();
     }
     printf("hello %s!\n", name);
